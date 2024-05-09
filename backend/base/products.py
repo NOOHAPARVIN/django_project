4697dd4c72ee76bb41be6820716c90adf9111272
NOOHAPARVIN <noohasageer@gmail.com>
2024-05-09 08:08:20 +0530

    backend folder

---
+++ backend/base/products.py
@@ -0,0 +1,28 @@
+products = [ 
+            {
+    '_id': '1',
+    'name' : 'airpods',
+    'image' : '/images/airpod.jpg',
+    'description' : 'airpods are newest technology',
+    'brand' : 'apple',
+    'price' : '999',
+    'category' : 'electronics',
+    'countInStock' : '10',
+    'name' : 'airpods',
+    'rating' : '4.5',
+    'numreviews' : '12',
+    },
+            {
+         '_id': '2',
+    'name' : 'mobilephone',
+    'image' : '/images/phone.jpg',
+    'description' : ' latest mobile phone',
+    'brand' : 'samsung',
+    'price' : '25000',
+    'name' : 'airpods',
+    'countInStock' : '10',
+     'category' : 'electronics',
+    'rating' : '4.5',
+    'numreviews' : '20',
+
+    }]
---
+++ backend/base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.getRoutes,name="routes"),
+     path('products/',views.getproducts,name="products"),
+    path('products/<str:pk>',views.getproduct,name="product")
+   
+]
